c82fbf78346d67662b76ca3a506defd4a38f7a0f
Ludo310 <157888273+Ludo310@users.noreply.github.com>
2025-03-17 19:29:08 +0100

    Update monitor.sh
    Réorganisation résultat

--- monitor.sh
+++ monitor.sh
@@ -8,24 +8,31 @@ fi
 # Couleurs
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+YELLOW='\033[1;33m'
 CYAN='\033[0;36m'
 NC='\033[0m'
 
+### Vérification des connexions actives ###
+list_active_connections() {
+  echo -e "${YELLOW}\n===== Connexions en cours =====${NC}"
+  netstat -tupan | grep ESTABLISHED | awk '{print $5, $6, $7}' | column -t
+}
+
 ### Vérification des services actifs ###
 list_active_services() {
-  echo -e "${CYAN}Services réseau actifs :${NC}"
+  echo -e "${CYAN}\n===== Services réseau actifs =====${NC}"
   systemctl list-units --type=service --state=running | grep -E 'ssh|xrdp|nginx|ftp|rdp|mysql|postgresql|apache|vsftpd'
 }
 
 ### Vérification des ports ouverts ###
 list_open_ports() {
-  echo -e "${CYAN}Ports en écoute et services associés :${NC}"
+  echo -e "${CYAN}\n===== Ports en écoute et services associés =====${NC}"
   ss -tulpen | grep 'LISTEN' | awk '{print $1, $5, $7}' | column -t
 }
 
 ### Vérification des règles de pare-feu ###
 list_firewall_rules() {
-  echo -e "${CYAN}Règles du pare-feu :${NC}"
+  echo -e "${RED}\n===== Règles du pare-feu =====${NC}"
   if command -v ufw &> /dev/null && sudo ufw status | grep -q "Status: active"; then
     sudo ufw status verbose
   else
@@ -33,22 +40,16 @@ list_firewall_rules() {
   fi
 }
 
-### Vérification des connexions actives ###
-list_active_connections() {
-  echo -e "${CYAN}Connexions en cours :${NC}"
-  netstat -tupan | grep ESTABLISHED | awk '{print $5, $6, $7}' | column -t
-}
-
 ### Dernières connexions extérieures ###
 list_last_connections() {
-  echo -e "${CYAN}15 dernières connexions externes :${NC}"
+  echo -e "${YELLOW}\n===== 15 dernières connexions externes =====${NC}"
   last -i | head -n 15
 }
 
 ### Exécution des fonctions ###
-echo -e "${CYAN}Analyse du système en cours...${NC}"
+echo -e "${GREEN}\n===== Analyse du système en cours... =====${NC}"
+list_active_connections
 list_active_services
 list_open_ports
 list_firewall_rules
-list_active_connections
 list_last_connections
